c2cf15ec8a1dbf0eee2a5294f38db43c88d049d0
longpv <long05062000@gmail.com>
2026-01-13 23:44:12 +0700

    loại bỏ bớt warring khi chạy code

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,13 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
 const browserSync = require("browser-sync").create();
-const path = require("path"); // Thêm path để xử lý unlink ảnh
+const path = require("path");
 
-// Đường dẫn nguồn và đích
+const SASS_OPTIONS = {
+	quietDeps: true,
+	silenceDeprecations: ["import", "global-builtin", "color-functions", "legacy-js-api"],
+};
+
 const paths = {
 	html: {
 		src: ["src/html/**/*.html", "!src/html/partials/**"],
@@ -43,12 +47,10 @@ const paths = {
 	},
 };
 
-// Xóa thư mục dist
 function clean() {
 	return del(["dist"]);
 }
 
-// Xử lý HTML
 function html() {
 	return src(paths.html.src)
 		.pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
@@ -56,44 +58,42 @@ function html() {
 		.pipe(browserSync.stream());
 }
 
-// Xử lý CSS
 function styles() {
-	return src(paths.styles.src).pipe(sass().on("error", sass.logError)).pipe(cleanCSS()).pipe(rename("main.css")).pipe(dest(paths.styles.dest)).pipe(browserSync.stream());
+	return src(paths.styles.src).pipe(sass(SASS_OPTIONS).on("error", sass.logError)).pipe(cleanCSS()).pipe(rename("main.css")).pipe(dest(paths.styles.dest)).pipe(browserSync.stream());
 }
 
-// Xử lý vendor CSS
 function vendorStyles() {
 	return src(paths.vendorStyles.src)
-		.pipe(sass({ url: false }).on("error", sass.logError))
+		.pipe(
+			sass({
+				...SASS_OPTIONS,
+				url: false,
+			}).on("error", sass.logError)
+		)
 		.pipe(cleanCSS())
 		.pipe(rename("vendor.css"))
 		.pipe(dest(paths.vendorStyles.dest))
 		.pipe(browserSync.stream());
 }
 
-// Xử lý JS
 function scripts() {
 	return src(paths.scripts.src).pipe(uglify()).pipe(rename("main.js")).pipe(dest(paths.scripts.dest)).pipe(browserSync.stream());
 }
 
-// Bundle vendor JS
 function vendorScripts() {
 	return src(paths.vendorScripts.src).pipe(concat("vendor.js")).pipe(dest(paths.vendorScripts.dest));
 }
 
-// Xử lý ảnh
 function images() {
 	return src(paths.images.src).pipe(imagemin()).pipe(dest(paths.images.dest)).pipe(browserSync.stream());
 }
 
-// Xử lý xóa ảnh
 function removeDeletedImage(filePath) {
 	const filePathFromSrc = path.relative(path.resolve("src/images"), filePath);
 	const destFilePath = path.resolve(paths.images.dest, filePathFromSrc);
 	del(destFilePath);
 }
 
-// Serve + Watch
 function serve() {
 	browserSync.init({
 		server: { baseDir: "dist" },
@@ -113,7 +113,6 @@ function serve() {
 	imageWatcher.on("unlink", removeDeletedImage);
 }
 
-// Tasks
 exports.clean = clean;
 exports.build = series(clean, parallel(html, styles, vendorStyles, scripts, vendorScripts, images));
 exports.default = series(exports.build, serve);
